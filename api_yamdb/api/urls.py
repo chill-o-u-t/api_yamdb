98dcff2e1b9f0712ac2d08ee902a7eb25a154e47
Ксения Кажура <keizha@mail.ru>
2022-05-31 16:56:44 +0300

    Add feature title(draft version)

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,12 +1,13 @@
 from rest_framework.routers import SimpleRouter
 from django.urls import path, include
 
-from api.views import GenreViewSet, CategoryViewSet
+from api.views import GenreViewSet, CategoryViewSet, TitleViewSet
 
 
 router = SimpleRouter()
 router.register('genres', GenreViewSet, basename='genres')
 router.register('categories', CategoryViewSet, basename='categories')
+router.register('titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('', include(router.urls)),
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,36 +1,45 @@
 from django.contrib.auth import get_user_model
 from django.db import models
-from core.models import  SortModel
+from core.models import SortModel
 
 User = get_user_model()
-   
+
+
 class Genre(SortModel):
     pass
-    
+
+
 class Category(SortModel):
     pass
 
 
 class Title(models.Model):
     name = models.CharField('Название', max_length=150)
-    year = models.DateField('Год выхода')
-    description = models.TextField('Описание')
-    genre = models.ForeignKey(
+    year = models.IntegerField('Год выхода')
+    description = models.TextField('Описание', blank=True)
+    genre = models.ManyToManyField(
         Genre,
         verbose_name='Жанр',
-        on_delete=models.SET_NULL,
         related_name='genre',
         blank=True,
-        null=True,
+        through='GenreTitle'
     )
     category = models.ForeignKey(
         Category,
         verbose_name='Категория',
-        on_delete=models.SET_NULL,
+        on_delete=models.PROTECT,
         related_name='category',
         blank=True,
         null=True,
     )
+
+
+class GenreTitle(models.Model):
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.genre} {self.title}'
 
 
 class Review(models.Model):
