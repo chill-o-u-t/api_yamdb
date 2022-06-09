6d82de047842b891b7403981f53626fa982de53e
Максим Близнюк <bmu0@yandex.ru>
2022-06-09 18:56:04 +0300

    removed_commented_code

--- api_yamdb/api/tokens.py
+++ api_yamdb/api/tokens.py
@@ -1,17 +1,5 @@
 from rest_framework_simplejwt.tokens import AccessToken
 
-'''
-class TokenGenerator(PasswordResetTokenGenerator):
-    def _make_hash_value(self, user, timestamp):
-        return (
-            six.text_type(user.pk) + six.text_type(timestamp)
-            + six.text_type(user.is_active)
-        )
-
-
-account_activation_token = TokenGenerator()
-'''
-
 
 def get_tokens_for_user(user):
     return {
